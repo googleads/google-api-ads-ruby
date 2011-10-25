94773a0f12bdac3f903be01c87bb8a5d4b1ce9c0
api.dklimkin@gmail.com <api.dklimkin@gmail.com@e942aa90-a85c-9e45-dd69-c8b10d4746d2>
2011-10-25 16:28:20 +0000

    Version bump for common
    git-svn-id: https://google-api-ads-ruby.googlecode.com/svn/trunk@160 e942aa90-a85c-9e45-dd69-c8b10d4746d2

--- ads_common/lib/ads_common/savon_headers/base_header_handler.rb
+++ ads_common/lib/ads_common/savon_headers/base_header_handler.rb
@@ -88,7 +88,7 @@ module AdsCommon
         app_name = credentials[:user_agent]
         # We don't know the library version here. A breaking change needs to be
         # introduced. This is scheduled for 0.6.0, using Common version for now.
-        lib_version = '0.5.3'
+        lib_version = '0.5.4'
         soap_user_agent = "Common-Ruby-%s; %s" % [lib_version, app_name]
         return "Savon/%s (%s)" % [Savon::Version, soap_user_agent]
       end
