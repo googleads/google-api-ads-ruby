1420bcb308b337c6d46545d255c7e05f47402dab
Danial Klimkin <api.dklimkin@gmail.com>
2013-07-19 16:21:56 +0400

    Post-release version bump

--- ads_common/lib/ads_common/version.rb
+++ ads_common/lib/ads_common/version.rb
@@ -21,6 +21,6 @@
 
 module AdsCommon
   module ApiConfig
-    CLIENT_LIB_VERSION = '0.9.3'
+    CLIENT_LIB_VERSION = '0.9.4'
   end
 end
--- adwords_api/lib/adwords_api/version.rb
+++ adwords_api/lib/adwords_api/version.rb
@@ -21,6 +21,6 @@
 
 module AdwordsApi
   module ApiConfig
-    CLIENT_LIB_VERSION = '0.9.3'
+    CLIENT_LIB_VERSION = '0.9.4'
   end
 end
