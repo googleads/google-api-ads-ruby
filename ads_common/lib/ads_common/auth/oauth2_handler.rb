a79af9ddb1b2741cc9aae4787d0752c25da6e92d
ramdanplusplus <dadan@41studio.com>
2015-09-24 13:31:34 +0700

    Update oauth2_handler.rb

--- ads_common/lib/ads_common/auth/oauth2_handler.rb
+++ ads_common/lib/ads_common/auth/oauth2_handler.rb
@@ -80,7 +80,11 @@ module AdsCommon
       # Overrides base get_token method to account for the token expiration.
       def get_token(credentials = nil)
         token = super(credentials)
-        token = refresh_token! if !@client.nil? && @client.expired?
+        #token = refresh_token! if !@client.nil? && @client.expired?
+        unless @client.nil?
+          @client.issued_at = Time.parse(@client.issued_at) if @client.issued_at.class.name == 'String'
+          token = refresh_token! if @client.expired?
+        end
         return token
       end
 
