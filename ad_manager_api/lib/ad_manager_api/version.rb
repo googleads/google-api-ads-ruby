949fb9bec7c82da004e6a1eab3d14ab178879c60
Chris Seeley <cseeley@google.com>
2025-08-13 18:46:49 +0000

    Release v4.2.0

--- ad_manager_api/lib/ad_manager_api/version.rb
+++ ad_manager_api/lib/ad_manager_api/version.rb
@@ -19,6 +19,6 @@
 
 module AdManagerApi
   module ApiConfig
-    CLIENT_LIB_VERSION = '4.1.0'
+    CLIENT_LIB_VERSION = '4.2.0'
   end
 end
--- ad_manager_api/test/bugs/test_issue_00000007.rb
+++ ad_manager_api/test/bugs/test_issue_00000007.rb
@@ -25,7 +25,7 @@ require 'test/unit'
 require 'ads_common/config'
 require 'ads_common/savon_service'
 require 'ads_savon/soap/response'
-require 'ad_manager_api/v202505/line_item_service'
+require 'ad_manager_api/v202508/line_item_service'
 
 class HeaderHandler
   def prepare_request(http, soap)
@@ -45,11 +45,11 @@ class StubService7 < AdsCommon::SavonService
   end
 
   def get_module()
-    return AdManagerApi::V202505::LineItemService
+    return AdManagerApi::V202508::LineItemService
   end
 
   def get_service_registry()
-    return AdManagerApi::V202505::LineItemService::LineItemServiceRegistry
+    return AdManagerApi::V202508::LineItemService::LineItemServiceRegistry
   end
 end
 
@@ -61,8 +61,8 @@ class TestAdManagerIssue7 < Test::Unit::TestCase
 
   TEST_NAMESPACE = 'https://ads.google.com/apis/ads/publisher/'
   TEST_ENDPOINT =
-      'https://ads.google.com/apis/ads/publisher/v202505/LineItemService?wsdl'
-  TEST_VERSION = :v202505
+      'https://ads.google.com/apis/ads/publisher/v202508/LineItemService?wsdl'
+  TEST_VERSION = :v202508
 
   def test_issue_7_request()
     args = {:line_items => [
@@ -95,7 +95,7 @@ class TestAdManagerIssue7 < Test::Unit::TestCase
   def get_xml_response_text()
     return <<EOT
   <soap:Body>
-    <getLineItemsByStatementResponse xmlns="https://www.google.com/apis/ads/publisher/v202505">
+    <getLineItemsByStatementResponse xmlns="https://www.google.com/apis/ads/publisher/v202508">
       <rval>
         <totalResultSetSize>1</totalResultSetSize>
         <startIndex>0</startIndex>
